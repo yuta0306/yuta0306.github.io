66be9f4a395e8490a6ed4ccc349b690a5dfc4ec0
Yubo <h31j027@ebwin.cis.iwate-u.ac.jp>
2021-05-16 00:40:56 +0900

    fix routine.js logger

--- utils/routine.js
+++ utils/routine.js
@@ -2,6 +2,7 @@ import { generateSitemap } from './generateSitemap.js'
 import { generateRobotsTxt } from './generateRobots.js'
 
 // Main Routine
+console.log('Generate some files before the build...')
 const sitemap = generateSitemap('https://yuta0306.github.io')
 const robotsTxt = generateRobotsTxt(sitemap)
-console.log(robotsTxt)
+console.log('completed!')
