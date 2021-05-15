697a75d740abb47579a7f7845ed2bcaf33a0317a
Yubo <h31j027@ebwin.cis.iwate-u.ac.jp>
2021-05-16 00:20:52 +0900

    sitemap generator

---
+++ utils/generateSitemap.js
@@ -0,0 +1,54 @@
+import fs from 'fs'
+import path from 'path'
+
+export function generateSitemap(
+    siteUrl,
+    top = '.next/server/pages',
+    out = 'public',
+    excludes = [],
+) {
+    let pages = [], currentDir = top, stackDir = []
+    var {childrenDir, childrenFiles} = getChildren(currentDir)
+    stackDir.push(...childrenDir)
+    pages.push(...childrenFiles)
+    while (stackDir.length > 0) {
+        currentDir = stackDir.pop()
+        var {childrenDir, childrenFiles} = getChildren(currentDir)
+        stackDir.push(...childrenDir)
+        pages.push(...childrenFiles)
+    }
+    
+    pages = pages.filter(page => !excludes.includes(page))
+    pages = pages.filter(page => page.match(/(\.html)$/))
+    pages = pages.map(page => path.join(siteUrl, page.replace(top, '')))
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">   
+${pages
+    .map(page => {
+    const path = page
+        .replace('/index', '')
+        .replace('.html', '')
+    return `
+    <url>
+        <loc>${path}</loc>
+    </url>
+            `
+    })
+    .join('')}
+</urlset>
+`
+    fs.writeFileSync(path.join(out, 'sitemap.xml'), sitemap)
+
+    return sitemap
+}
+
+const getChildren = (currentDir) => {
+    const children = fs.readdirSync(currentDir)
+    const childrenDir = children.filter(value => fs.statSync(path.join(currentDir, value)).isDirectory())
+                            .map(dir => path.join(currentDir, dir))
+    const childrenFiles = children.filter(value => fs.statSync(path.join(currentDir, value)).isFile())
+                            .map(file => path.join(currentDir, file))
+    
+    return {childrenDir, childrenFiles}
+}
---
+++ utils/routine.js
@@ -0,0 +1,4 @@
+import { generateSitemap } from './generateSitemap.js'
+
+// Main Routine
+const sitemap = generateSitemap('https://yuta0306.github.io')
