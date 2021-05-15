c153afbd3d7095fdce06e1a35e7619dfde9e9916
yuta0306 <yuta0306@users.noreply.github.com>
2021-05-15 16:07:21 +0000

    deploy: 81d6fdafb90f91664242cac823179e6077928710

---
+++ _next/static/kfJKyjxmWpuJs08_kxdEB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002F","\u002Fprivacy-policy","\u002F[slug]","\u002Fcategory\u002F[category]\u002F[page]","\u002Fpost\u002F[page]","\u002Ftag\u002F[tag]\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
