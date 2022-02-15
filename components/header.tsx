5aa5eeff6404f99c6d5fd967dda4599cf7db6fab
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-02-15 12:21:35 +0900

    add katex

--- components/header.tsx
+++ components/header.tsx
@@ -1,76 +1,75 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import React, {useState} from 'react'
-
-import { siteName, contactForm } from '../global.d'
+import React, { useState } from 'react'
+import { contactForm, siteName } from '../global.d'
 import styles from './header.module.scss'
 
-export default function Header({index='blog', categories=[]}:
+
+export default function Header({ index = 'blog', categories = [] }:
     {
         index?: string,
         categories?: Array<string>,
     }
-    ) {
+) {
     const [openMenu, setOpenMenu] = useState(false)
     const menuToggle = () => {
         setOpenMenu(!openMenu)
     }
-    
+
     return (
         <>
-        <div className={styles.container}>
-            <div className={styles.container__inner}>
-                <header className={styles.header}>
-                    <Link href="/">
-                        <a>
-                            {index == 'index'
-                                ? <h1 className={styles.header__title}>{siteName}</h1>
-                                : <div className={styles.header__title}>{siteName}</div>
-                            }
-                        </a>
-                    </Link>
-                </header>
-                <div className={styles.hamburger} onClick={() => menuToggle()}>
-                    <div>
-                        <img src='/icons/hamburger.png' />
+            <div className={styles.container}>
+                <div className={styles.container__inner}>
+                    <header className={styles.header}>
+                        <Link href="/">
+                            <a>
+                                {index == 'index'
+                                    ? <h1 className={styles.header__title}>{siteName}</h1>
+                                    : <div className={styles.header__title}>{siteName}</div>
+                                }
+                            </a>
+                        </Link>
+                    </header>
+                    <div className={styles.hamburger} onClick={() => menuToggle()}>
+                        <div>
+                            <img src='/icons/hamburger.png' />
+                        </div>
                     </div>
-                    </div>
-                <nav className={openMenu ? styles.nav : styles.nav__closed}>
-                    <ul className={styles.nav__list}>
-                        <li className={index == 'about' ? styles.nav__item_active : styles.nav__item} key='about'>
-                            <Link href="/about">
-                                <a>About</a>
-                            </Link>
-                        </li>
-                        <li className={index == 'blog' || index == 'index' ? styles.nav__item_active : styles.nav__item} key='blog'> 
-                            <Link href="/">
-                                <a>Blog</a>
-                            </Link>
-                        </li>
-                        <li className={index == 'contact' ? styles.nav__item_active : styles.nav__item} key='contact'>
-                            <Link href={contactForm ? contactForm : "/"}>
-                                <a>Contact</a>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
+                    <nav className={openMenu ? styles.nav : styles.nav__closed}>
+                        <ul className={styles.nav__list}>
+                            <li className={index == 'about' ? styles.nav__item_active : styles.nav__item} key='about'>
+                                <Link href="/about">
+                                    <a>About</a>
+                                </Link>
+                            </li>
+                            <li className={index == 'blog' || index == 'index' ? styles.nav__item_active : styles.nav__item} key='blog'>
+                                <Link href="/">
+                                    <a>Blog</a>
+                                </Link>
+                            </li>
+                            <li className={index == 'contact' ? styles.nav__item_active : styles.nav__item} key='contact'>
+                                <Link href={contactForm ? contactForm : "/"}>
+                                    <a>Contact</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
-        <div className={styles.category}>
-            <ul className={styles.category__items}>
-                {
-                    categories.map(category => {
-                        return (
-                            <Link href={`/category/${category}/1`}>
-                                <a className={styles.category__item}>
-                                    <li key={category}>{category}</li>
-                                </a>
-                            </Link>
-                        )
-                    })
-                }
-            </ul>
-        </div>
+            <div className={styles.category}>
+                <ul className={styles.category__items}>
+                    {
+                        categories.map(category => {
+                            return (
+                                <Link href={`/category/${category}/1`}>
+                                    <a className={styles.category__item}>
+                                        <li key={category}>{category}</li>
+                                    </a>
+                                </Link>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </>
     )
 }
--- components/partial/card.tsx
+++ components/partial/card.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { author } from '../../global.d';
+import styles from './card.module.scss';
 
-import styles from './card.module.scss'
-import {author} from '../../global.d'
 
-export default function Card({slug, content} : {
+export default function Card({ slug, content }: {
     slug: string,
     content: any
 }) {
@@ -13,16 +13,16 @@ export default function Card({slug, content} : {
     }
 
     return (
-        <div className={styles.card__container}>
+        <div className={styles.card__container} key={slug.toString()}>
             <Link href={`/${slug}`}>
                 <a>
                     <div className={styles.card}>
                         <div className={styles.card__thumbnail}>
                             {content.Thumbnail ?
-                                <img src={content.Thumbnail}  alt={content.Title}
+                                <img src={content.Thumbnail} alt={content.Title}
                                     className={styles.card__thumbnail__img} loading='lazy' />
                                 :
-                                <img src='/images/default.png'  alt={content.Title}
+                                <img src='/images/default.png' alt={content.Title}
                                     className={styles.card__thumbnail__img} loading='lazy' />
                             }
                         </div>
@@ -31,20 +31,20 @@ export default function Card({slug, content} : {
                                 <time dateTime={content.Date} itemProp='published'>{content.Date}</time>
                                 {
                                     sameAuthor ?
-                                    <Link href="/about">
-                                        <a itemScope={true} role='author' itemType='http://schema.org/Person'>
-                                            <span itemProp='name'>{content.Author || author}</span>
-                                        </a>
-                                    </Link>
-                                    :
-                                    <span itemScope={true} role='author'
-                                        itemType='http://schema.org/Person'
-                                        itemProp='name'>{content.Author || author}</span>
+                                        <Link href="/about">
+                                            <a itemScope={true} role='author' itemType='http://schema.org/Person'>
+                                                <span itemProp='name'>{content.Author || author}</span>
+                                            </a>
+                                        </Link>
+                                        :
+                                        <span itemScope={true} role='author'
+                                            itemType='http://schema.org/Person'
+                                            itemProp='name'>{content.Author || author}</span>
                                 }
                             </div>
                             <h2 className={styles.card__title}>{content.Title}</h2>
                         </div>
-                    
+
                     </div>
                 </a>
             </Link>
