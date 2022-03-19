f855a4fdd95fd8857bc76fcb48dc625b6f8fa980
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-03-19 22:05:55 +0900

    update design

--- components/partial/shortbio.tsx
+++ components/partial/shortbio.tsx
@@ -1,6 +1,6 @@
 import styles from './shortbio.module.scss'
 
-export default function ShortBio({bio, author} : {
+export default function ShortBio({ bio, author }: {
     bio: string,
     author: string
 }) {
@@ -8,8 +8,8 @@ export default function ShortBio({bio, author} : {
         return <p className={styles.container__paragraph}>{paragraph}</p>
     })
     return (
-        <div className={styles.container}  itemScope={true} itemProp='author'
-            itemType='http://schema.org/Person'>
+        <div className={styles.container} itemScope={true} itemProp='author'
+            itemType='http://schema.org/Person' key='short-bio'>
             <div className={styles.container__image}>
                 <img src='/images/profile.jpeg' alt={author} loading='lazy' />
             </div>
