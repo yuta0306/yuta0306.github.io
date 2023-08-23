e36c5dbe7144e38e514df62891c685de0cee58ba
yuta0306 <yuta.20010306@gmail.com>
2023-08-24 00:51:24 +0900

    add rss

---
+++ utils/genearateRSS.js
@@ -0,0 +1,75 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeMathJax from 'rehype-mathjax'
+import rehypeStringify from 'rehype-stringify'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import RSS from 'rss'
+import unified from 'unified'
+// import { siteDescription, siteName, siteUrl } from '../global.d'
+
+async function getPosts(dirName) {
+    const fileNames = fs.readdirSync(dirName)
+    const allPostData = fileNames.map(fileName => {
+        const slug = fileName.replace(/\.md$/, '')
+        const fullPath = path.join(dirName, fileName)
+        const content = _getMd2Html(fullPath)
+
+        return {
+            slug,
+            content,
+        }
+    })
+
+    return allPostData
+}
+
+function _getMd2Html(fileName) {
+    const fileContent = fs.readFileSync(fileName, 'utf8')
+    const matterResult = matter(fileContent)
+
+    const processedContent = unified()
+        .use(remarkParse, { fragment: true })
+        .use(remarkMath)
+        .use(remarkGfm)
+        .use(remarkRehype)
+        .use(rehypeMathJax)
+        .use(rehypeHighlight)
+        .use(rehypeStringify)
+        .process(matterResult.content)
+
+    const contentHtml = processedContent.toString()
+
+    return {
+        contentHtml,
+        ...matterResult.data
+    }
+}
+
+export async function generateRSS(dirName, siteName, siteDescription, siteUrl) {
+    const feed = new RSS({
+        title: siteName,
+        description: siteDescription,
+        site_url: siteUrl,
+        feed_url: path.join(siteUrl, "feed"),
+        language: 'ja',
+    });
+
+    // 例としてpostsを含めるイメージ
+    // このあたりの書き方はライブラリのドキュメントを参考にしてください
+    const posts = await getPosts(dirName);
+    posts?.forEach(({ slug, content }) => {
+        feed.item({
+            title: content.Title,
+            description: content.Description,
+            date: new Date(content.Date),
+            url: path.join(siteUrl, slug),
+        });
+    })
+    const xml = feed.xml();
+    fs.writeFileSync(path.join("public", "feed.xml"), xml)
+}
--- utils/routine.js
+++ utils/routine.js
@@ -1,8 +1,13 @@
+import { generateRSS } from './genearateRSS.js'
 import { generateRobotsTxt } from './generateRobots.js'
 import { generateSitemap } from './generateSitemap.js'
 
 // Main Routine
+const siteName = 'ゆうぼうの書跡棚'
+const siteUrl = 'https://yuta0306.github.io'
+const siteDescription = 'スキルや知識をつけて将来ナマケモノになるまでの技術ブログです．主に，機械学習やPython, JavaScriptによる開発についてまとめます．'
 console.log('Generate some files before the build...')
 const sitemap = generateSitemap('https://yuta0306.github.io')
 const robotsTxt = generateRobotsTxt(sitemap)
+await generateRSS("pages/docs", siteName, siteDescription, siteUrl)
 console.log('completed!')
