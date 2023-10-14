f4702dcd70a290e70ea2a57e6e0a05ad1f1654cb
yuta0306 <yuta.20010306@gmail.com>
2023-10-14 22:31:26 +0900

    update profile

--- utils/routine.js
+++ utils/routine.js
@@ -5,7 +5,7 @@ import { generateSitemap } from './generateSitemap.js'
 // Main Routine
 const siteName = 'ゆうぼうの書跡棚'
 const siteUrl = 'https://yuta0306.github.io'
-const siteDescription = 'スキルや知識をつけて将来ナマケモノになるまでの技術ブログです．主に，機械学習やPython, JavaScriptによる開発についてまとめます．'
+const siteDescription = 'スキルや知識をつけて将来ナマケモノになるまでの技術ブログです．主に，機械学習やPython, JavaScriptによる開発，読んだ論文についてまとめます．'
 console.log('Generate some files before the build...')
 const sitemap = generateSitemap('https://yuta0306.github.io')
 const robotsTxt = generateRobotsTxt(sitemap)
